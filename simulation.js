becfa81180a5249c4432aff28ffb1ee48048358b
Mayank <mayanks23@iiserb.ac.in>
2026-06-14 01:29:51 +0530

    Add files via upload

--- simulation.js
+++ simulation.js
@@ -9,11 +9,9 @@ class MicrobiomeRLSimulation {
         this.b1 = 900; 
         this.time = 0;
         
-        this.qValues = {};
-        for (let i = 0; i < this.params.numMeals; i++) {
-            this.qValues[i] = 0;
-        }
+        this.qTable = {};
         this.currentAction = Math.floor(this.params.numMeals / 2);
+        this.prevStateKey = null;
         
         this.history = {
             time: [], b0: [], b1: [], diversity: [], kick: [], 
@@ -23,6 +21,21 @@ class MicrobiomeRLSimulation {
         for (let i = 0; i < this.params.numMeals; i++) {
             this.history.qValues[i] = [];
         }
+    }
+
+    getStateKey(b0, b1, kick, diversity) {
+        const b0Bin = Math.min(9, Math.floor(b0 / 100));
+        const b1Bin = Math.min(9, Math.floor(b1 / 100));
+        const kickBin = Math.min(9, Math.floor(kick / 20));
+        const divBin = Math.min(6, Math.floor(diversity * 10)); 
+        return `${b0Bin}_${b1Bin}_${kickBin}_${divBin}`;
+    }
+
+    getQValues(stateKey) {
+        if (!this.qTable[stateKey]) {
+            this.qTable[stateKey] = new Array(this.params.numMeals).fill(0);
+        }
+        return this.qTable[stateKey];
     }
 
     calculateShannonDiversity() {
@@ -39,8 +52,9 @@ class MicrobiomeRLSimulation {
     step() {
         if (this.time >= this.params.maxSteps) return null;
 
-        const food = this.params.meals[this.currentAction];
         const p = this.params;
+
+        const food = this.params.meals[this.currentAction];
         const totalPop = this.b0 + this.b1;
 
         const b0Birth = this.b0 * p.b0Growth * (1 + food.alcohol * 4);
@@ -60,6 +74,18 @@ class MicrobiomeRLSimulation {
 
         const kick = this.b0 * p.chemProd;
         const reward = kick > p.kickThreshold ? kick * 1.5 : kick;
+        const diversity = this.calculateShannonDiversity();
+
+        const currentStateKey = this.getStateKey(this.b0, this.b1, kick, diversity);
+        const currentQValues = this.getQValues(currentStateKey);
+
+        if (this.time > 0 && this.prevStateKey !== null) {
+            const prevQValues = this.getQValues(this.prevStateKey);
+            const maxNextQ = Math.max(...currentQValues);
+
+            prevQValues[this.currentAction] += p.learningRate * 
+                (reward + p.discount * maxNextQ - prevQValues[this.currentAction]);
+        }
 
         let nextAction;
         if (Math.random() < p.epsilon) {
@@ -68,42 +94,35 @@ class MicrobiomeRLSimulation {
             let maxQ = -Infinity;
             nextAction = 0;
             for (let i = 0; i < p.numMeals; i++) {
-                if (this.qValues[i] > maxQ) {
-                    maxQ = this.qValues[i];
+                if (currentQValues[i] > maxQ) {
+                    maxQ = currentQValues[i];
                     nextAction = i;
                 }
             }
         }
 
-        let maxNextQ = -Infinity;
-        for (let i = 0; i < p.numMeals; i++) {
-            if (this.qValues[i] > maxNextQ) maxNextQ = this.qValues[i];
-        }
-        
-        this.qValues[this.currentAction] += p.learningRate * 
-            (reward + p.discount * maxNextQ - this.qValues[this.currentAction]);
-
+        this.prevStateKey = currentStateKey;
         this.currentAction = nextAction;
 
         const currentTotal = this.b0 + this.b1;
         this.history.time.push(this.time);
         this.history.b0.push(this.b0);
         this.history.b1.push(this.b1);
-        this.history.diversity.push(this.calculateShannonDiversity());
+        this.history.diversity.push(diversity);
         this.history.kick.push(kick);
         this.history.actions.push(this.currentAction);
         this.history.b0Proportion.push(this.b0 / currentTotal);
         
         for (let i = 0; i < p.numMeals; i++) {
-            this.history.qValues[i].push(this.qValues[i]);
+            this.history.qValues[i].push(currentQValues[i]);
         }
 
         this.time++;
 
         return {
             time: this.time, b0: this.b0, b1: this.b1,
-            diversity: this.calculateShannonDiversity(), kick: kick,
-            action: this.currentAction, qValues: { ...this.qValues },
+            diversity: diversity, kick: kick,
+            action: this.currentAction, qValues: currentQValues,
             b0Proportion: this.b0 / currentTotal
         };
     }
